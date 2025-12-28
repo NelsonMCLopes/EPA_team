76330b72c4ffb895bff2740d9bb327e99a071890
NelsonMCLopes <63874611+NelsonMCLopes@users.noreply.github.com>
2025-12-28 22:17:24 +0000

    Deploying to gh-pages from @ NelsonMCLopes/EPA_team@18187d3431427558baa901efd31b1c749093a3e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/team/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/research/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
